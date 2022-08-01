02d07645bfcc3d449bd4c243a4599571478667db
mr687 <davinomoehdanino@gmail.com>
2022-08-01 15:10:33 +0700

    add lowercase username validation

--- dto/register.dto.go
+++ dto/register.dto.go
@@ -2,7 +2,7 @@ package dto
 
 // Define form validation when user hit /register API
 type RegisterRequest struct {
-	Username string `json:"username" form:"username" binding:"required"`
+	Username string `json:"username" form:"username" binding:"required,lowercase"`
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password" form:"password" binding:"required"`
 }
