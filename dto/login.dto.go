79736bf8c20117d9b7915c7901ca8c5e3c527736
mr687 <davinomoehdanino@gmail.com>
2022-08-01 15:10:08 +0700

    edit login field to account

--- dto/login.dto.go
+++ dto/login.dto.go
@@ -2,6 +2,6 @@ package dto
 
 // Define form validation when user hit /login API
 type LoginCredential struct {
-	Email    string `json:"email" form:"email" binding:"required,email"`
+	Account  string `json:"account" form:"account" binding:"required"`
 	Password string `json:"password" form:"password" binding:"required"`
 }
